aa71c7f3c950a3e9dfeaa692f147eda386f3afbb
Isai Gonzalez08 <isaigonzalez482@gmail.com>
2025-04-01 21:29:34 -0600

    [ADD] login page

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div>
+      <h2 className="text-4xl">Bienvenido de vuelta</h2>
+      <p className="text-base">Ingresa tus datos para entrar.</p>
+    </div>
+  );
+}
